1ad1bf19478039691f4cb9d450969c5346801f08
dmrachk <dmrachk@bgsu.edu>
2022-04-20 12:31:39 -0400

    Munging and Replacement functions complete.

--- data_munge_funcs.R
+++ data_munge_funcs.R
@@ -1,5 +1,6 @@
 ### R function for data munging the hospital data.
 
+# function for doing the primary data munging (factors, drops, etc.)
 hosp_munge <- function(hospital) {
   hospital$hospital_death <- as.factor(hospital$hospital_death)
   levels(hospital$hospital_death) <- c("0", "1")
@@ -50,4 +51,79 @@ hosp_munge <- function(hospital) {
   
   return(hospital)
   
+}
+
+
+# function for replacing NA values of factors with the mode of the column
+hosp_mode_mean <- function(hospital) {
+  cols = length(hospital)
+  
+  # iterate over columns of data table
+  for (i in 1:cols) {
+    # check if column is a factor
+    if (is.factor(hospital[,i])) {
+      # iterate over values, compute mode, replace NAs with mode.
+      samp <- c()
+      for (j in 1:length(hospital[,i])) {
+        if (!is.na(hospital[j,i])) {
+          samp <- append(samp, hospital[j,i])
+        }
+      }
+      uniq_samp <- unique(samp)
+      mode_samp <- uniq_samp[which.max(tabulate(match(samp, uniq_samp)))]
+      hospital[,i] <- replace_na(hospital[,i], replace = mode_samp)
+    } else {
+      # in this case the values are numeric and need to be replaced with mean value
+      hospital[is.na(hospital[,i]),i] <- mean(hospital[,i], na.rm=TRUE)
+    }
+  }
+  
+  return(hospital)
+}
+
+
+### R function for creating 3 partitions with training, validation and testing set
+### Input: original dataset name (data)
+###        proportion of records assigned to training set (prop.train)
+###        proportion of records assigned to validation set (prop.val)
+### Output: data.train, data.val, data.test
+
+partition.3 <- function(data, prop.train, prop.val){
+  # select a random sample of size = prop.train % of total records
+  selected1 <- sample(1:nrow(data), round(nrow(data)*prop.train), replace = FALSE) 
+  # create training data which has prop.train % of total records
+  data.train <- data[selected1,]
+  # select a random sample of size = prop.val % of the total records
+  rest <- setdiff(1:nrow(data), selected1)
+  selected2 <- sample(rest, round(nrow(data)*prop.val), replace = FALSE) 
+  # create validation data which has prop.val % of total records
+  data.val <- data[selected2,]
+  # create testing data with the remaining records
+  data.test <- data[setdiff(rest, selected2),]
+  return(list(data.train=data.train, data.test=data.test, data.val=data.val))
+}
+
+
+### R function for creating 2 partitions with training and test set
+### Input: original dataset name (data)
+###        proportion of records assigned to training set (prop.train)
+### Output: data.train, data.test
+
+partition.2 <- function(data, prop.train){
+  # select a random sample of size = prop.train % of total records
+  selected <- sample(1:nrow(data), round(nrow(data)*prop.train), replace = FALSE) 
+  # create training data which has prop.train % of total records
+  data.train <- data[selected,]
+  # create validation data
+  rest <- setdiff(1:nrow(data), selected)
+  data.test <- data[rest,]
+  return(list(data.train=data.train, data.test=data.test))
+}
+
+
+### R function for calculating Euclidean distance
+
+eucl.dist <- function(X, Y){
+  dist <- sqrt(sum((X - Y)^2))
+  return(dist)
 }
--- drm_models.r
+++ drm_models.r
@@ -6,11 +6,59 @@ library(tidyr)
 # Load Raw hospital data
 hosp <- read.csv("dataset.csv")
 
-# Drop all NA values and save as new data set
+# Create 2 data sets (MICE data set created externally)
+# 1. MICE imputed data set
+# 2. Drop NA
+# 3. Mean/Mode Replaced sets
+#   3a. Mode used as replacement for factors
+#   3b. Mean used as replacement for numeric vals
 
 # First do some data munging
-hosp_na <- hosp_munge(hosp)
+hosp_drop <- hosp_munge(hosp)
+hosp_mm <- hosp_munge(hosp)
 
-# Drop
-hosp_na <- drop_na(hospital)
-# 
+# Drop na for dropped set
+hosp_drop <- drop_na(hospital)
+
+# Replace na with mode/mean for hosp_mm
+hosp_mm <- hosp_mode_mean(hosp_mm)
+
+
+# ==============================================================================
+# MODELS
+# Each model is built over each of the 3 data sets:
+# MICE: the MICE imputed data set
+# DROP: the NA dropped data set
+# MM  : the NA replaced with column-wise Means/Modes data set 
+
+
+# --------------------------
+# Classification Trees (CT)
+
+# CT_MICE
+
+# CT_DROP
+
+# CT_MM
+
+
+
+# ----------------------------------
+# Random Forest Classification (RF)
+
+# RF_MICE
+
+# RF_DROP
+
+# RF_MM
+
+
+
+# ----------------------------------------
+# Logistic Regression Classification (LR)
+
+# LR_MICE
+
+# LR_DROP
+
+# LR_MM
